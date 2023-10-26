42517a91c5c22672db2bde86a27937c312632b49
icebear2n2 <icebear2n2@gmail.com>
2023-10-26 11:20:48 +0900

    init: 프로젝트 초기 설정

---
+++ src/test/java/com/icebear2n2/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icebear2n2.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
